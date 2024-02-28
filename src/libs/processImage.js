072ac5ae4ecbf9078f1d0fe46fe2ba2e3d517d26
Dartrix137 <daniel.ftg@hotmail.com>
2024-02-28 18:27:36 -0500

    Uploading new changes when editing products

--- src/libs/processImage.js
+++ src/libs/processImage.js
@@ -4,7 +4,7 @@ import path from 'path'
 export default async function processImage(image) {
     const bytes = await image.arrayBuffer()
     const buffer = Buffer.from(bytes)
-    const filePath = path.join(process.cwd(), image.name)
+    const filePath = path.join(process.cwd(),'tmp', image.name)
     await writeFile(filePath, buffer)
     return filePath;
 }
