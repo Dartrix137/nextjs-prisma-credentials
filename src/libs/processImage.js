04a855db7aee7377ca69696cf66a28f4dd0e9f90
Dartrix137 <daniel.ftg@hotmail.com>
2024-02-29 17:55:29 -0500

    Uploading new changes when editing products

--- src/libs/processImage.js
+++ src/libs/processImage.js
@@ -5,5 +5,5 @@ export default async function processImage(image) {
     const bytes = await image.arrayBuffer()
     const buffer = Buffer.from(bytes).toString('base64');
     const withPrefix = 'data:image/png;base64,' + buffer;
-    return withPrefix;
+    return buffer;
 }
