5ece13a44f94fcef40d07f04c43f7836316494ea
Dartrix137 <daniel.ftg@hotmail.com>
2024-02-29 16:14:51 -0500

    Uploading new changes when editing products

--- src/libs/processImage.js
+++ src/libs/processImage.js
@@ -2,7 +2,7 @@ import {writeFile} from 'fs/promises'
 import {readFileSync} from 'fs'
 
 export default async function processImage(image) {
-    const byteArrayBuffer = readFileSync('sample.jpg');
+    const byteArrayBuffer = readFileSync(image);
     const bytes = await image.arrayBuffer()
     const buffer = Buffer.from(bytes)
     const filePath = '/tmp/'+image.name
