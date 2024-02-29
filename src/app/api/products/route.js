6b92d5f97e2e386055ad56dd04a517c7dd94bbe3
Dartrix137 <daniel.ftg@hotmail.com>
2024-02-29 15:56:04 -0500

    Uploading new changes when editing products

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -48,7 +48,7 @@ export async function POST(request) {
 
     const filePath = await processImage(image);
     console.log("filepath:"+filePath)
-    const res = await cloudinary.uploader.upload(image);
+    const res = await cloudinary.uploader.upload(process.cwd()+filePath);
     console.log(res)
     const result = await db.product.create({
       data: {
